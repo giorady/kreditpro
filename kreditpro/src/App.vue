33d95535a9a2ce80868f06a708457f33f808ba7c
giorady <muhammadgiorady@yahoo.com>
2023-08-14 07:46:02 +0700

    Modified & cleaned files

--- kreditpro/src/App.vue
+++ kreditpro/src/App.vue
@@ -2,14 +2,21 @@
   <nav class="navbar navbar-expand-lg bg-body-tertiary">
     <div class="container-fluid">
       <a class="navbar-brand" href="#">Kreditpro</a>
-      <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent"
-        aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
+      <button
+        class="navbar-toggler"
+        type="button"
+        data-bs-toggle="collapse"
+        data-bs-target="#navbarSupportedContent"
+        aria-controls="navbarSupportedContent"
+        aria-expanded="false"
+        aria-label="Toggle navigation"
+      >
         <span class="navbar-toggler-icon"></span>
       </button>
       <div class="collapse navbar-collapse" id="navbarSupportedContent">
         <ul class="navbar-nav me-auto mb-2 mb-lg-0">
           <li class="nav-item">
-            <router-link class="nav-link" to="/">Register</router-link>
+            <router-link class="nav-link" to="/register">Register</router-link>
           </li>
           <li class="nav-item">
             <router-link class="nav-link" to="/login">Login</router-link>
